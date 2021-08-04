bfeda74ef81a6e2802addc8632520b682f6dc61a
luizov <luizovdimitur@gmail.com>
2021-08-04 21:40:57 +0300

    Added a jsx sample snippet

--- samples/javascript-es5.js
+++ samples/javascript-es5.js
@@ -5,6 +5,82 @@ function sayHello(somebody) {
     alert('Hello ' + somebody);
 }
 
+// For loop
+for (let i = 0; i < 5; i++) {
+    // loop
+}
+
+
+// If/else.
+if (condition) {
+    //
+} else if (condition2) {
+    //
+} else {
+    //
+}
+
+var allTrue = every([
+    10 === 3,
+    1 === '1',
+    false
+])
+    
+let someTrue = _.some([
+    true,
+    typeof 2 == 'string'
+])
+
+// Class.
+class Dog extends Animal {
+    bark() {
+        console.log('Woof!')
+    }
+}
+
+export default Dog;
+
+export function greet(name) {
+    let message = `Hello ${name}`;
+    console.log(message);
+}
+
+greet('Foo');
+
+const https = require('https');
+
+/**
+ * Make request
+ *
+ * @param String URL
+ */
+const makeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
+
+            res.on('data', data => {
+                body += data;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(body));
+            });
+
+        }).on('error', reject);
+    });
+}
+
+async function getUser(id) {
+    let user = await makeRequest(`https://api.github.com/users/${id}`);
+    return user
+}
+
+getUser(4);
+
+const validFormat = /\d[a-z]\.*\d{4}/i;
+
 
 /** @type {function(string, boolean): number} Closure syntax */
 (function ($) {
